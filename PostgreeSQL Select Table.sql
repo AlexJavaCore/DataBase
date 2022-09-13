89259f59e5bf702846191a10e9f970827e946a0a
Alexander Smolin <alexjavacore@mail.ru>
2022-09-13 16:06:10 +0300

    Update PostgreeSQL Select Table.sql

--- PostgreeSQL Select Table.sql
+++ PostgreeSQL Select Table.sql
@@ -18,6 +18,6 @@ AND gdp/population >
 
 SELECT name, continent FROM world
 WHERE continent IN 
-(SELECT continent FROM world
-WHERE name IN ('Argentina', 'Australia'))
-ORDER BY name
+           (SELECT continent FROM world
+            WHERE name IN ('Argentina', 'Australia'))
+            ORDER BY name
