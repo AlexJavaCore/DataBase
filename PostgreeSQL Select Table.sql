b9d654fa66b2b033baf43878866e553290098ff4
Alexander Smolin <alexjavacore@mail.ru>
2022-09-13 19:34:55 +0300

    Update PostgreeSQL Select Table.sql

--- PostgreeSQL Select Table.sql
+++ PostgreeSQL Select Table.sql
@@ -32,3 +32,17 @@ WHERE population >
 AND population < 
       (SELECT population FROM world
        WHERE name = 'Germany') 
+
+-- #5 Germany (population 80 million) has the largest population of the countries in Europe.
+-- # Austria (population 8.5 million) has 11% of the population of Germany.
+-- # Show the name and the population of each country in Europe.
+-- # Show the population as a percentage of the population of Germany.
+-- # The format should be Name, Percentage for example:
+
+SELECT name, CONCAT(ROUND(population/
+      (SELECT population/100.0
+       FROM world
+       WHERE name = 'Germany')), '%') 
+FROM world 
+WHERE continent = 'Europe'
+
