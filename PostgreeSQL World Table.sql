014dd6fc35909f8fcfffe69538aeba03e148702c
Alexander Smolin <alexjavacore@mail.ru>
2022-09-13 15:03:40 +0300

    Update PostgreeSQL World Table.sql

--- PostgreeSQL World Table.sql
+++ PostgreeSQL World Table.sql
@@ -15,7 +15,8 @@ SELECT name, gdp/population
 FROM world
 WHERE population >= 200000000
 
--- #4. Show the name and population in millions for the countries of the continent 'South America'. Divide the population by 1000000 to get population in millions.
+-- #4. Show the name and population in millions for the countries of the continent 'South America'.
+-- # Divide the population by 1000000 to get population in millions.
 
 SELECT name, population/1000000
 FROM world
@@ -32,13 +33,15 @@ WHERE name IN ('France', 'Germany', 'Italy')
 SELECT name FROM world
 WHERE name LIKE '%United'
 
--- #7. Two ways to be big: A country is big if it has an area of more than 3 million sq km or it has a population of more than 250 million.
+-- #7. Two ways to be big: A country is big if it has an area of more than 3 million sq km
+-- # or it has a population of more than 250 million.
 
 SELECT name, population, area FROM world
 WHERE area > 3000000
 OR population > 250000000
 
--- #8. Exclusive OR (XOR). Show the countries that are big by area (more than 3 million) or big by population (more than 250 million) but not both. Show name, population and area.
+-- #8. Exclusive OR (XOR). Show the countries that are big by area (more than 3 million) or big by population
+-- #(more than 250 million) but not both. Show name, population and area.
 
 SELECT name, population, area
 FROM world
@@ -47,7 +50,8 @@ AND population < 250000000
 OR area < 3000000
 AND population > 250000000
 
--- #9. Show the name and population in millions and the GDP in billions for the countries of the continent 'South America'. Use the ROUND function to show the values to two decimal places.
+-- #9. Show the name and population in millions and the GDP in billions for the countries of the continent 'South America'. 
+-- # Use the ROUND function to show the values to two decimal places.
 
 SELECT name,
 ROUND(population/1000000.0, 2),
@@ -55,7 +59,8 @@ ROUND(gdp/1000000000.0, 2)
 FROM world
 WHERE continent = 'South America'
 
--- #10. Show the name and per-capita GDP for those countries with a GDP of at least one trillion (1000000000000; that is 12 zeros). Round this value to the nearest 1000.
+-- #10. Show the name and per-capita GDP for those countries with a GDP of at least one trillion (1000000000000; that is 12 zeros).
+-- # Round this value to the nearest 1000.
 
 SELECT name, Round(gdp/population, -3)
 FROM world
@@ -67,15 +72,17 @@ SELECT name, capital
 FROM world
 WHERE LENGTH(name) = LENGTH(capital)
 
--- #12. Show the name and the capital where the first letters of each match. Don't include countries where the name and the capital are the same word.
+-- #12. Show the name and the capital where the first letters of each match.
+-- # Don't include countries where the name and the capital are the same word.
 
 SELECT name, capital
 FROM world
 WHERE LEFT(name, 1) = LEFT(capital, 1)
 AND name <> capital
 
--- #13. Equatorial Guinea and Dominican Republic have all of the vowels (a e i o u) in the name. They don't count because they have more than one word in the name.
-# Find the country that has all the vowels and no spaces in its name.
+-- #13. Equatorial Guinea and Dominican Republic have all of the vowels (a e i o u) in the name. 
+-- # They don't count because they have more than one word in the name.
+-- # Find the country that has all the vowels and no spaces in its name.
 
 SELECT name
 FROM world
