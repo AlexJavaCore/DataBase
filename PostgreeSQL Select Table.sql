d2d64029cab76cd01be6274661d24631d5abfc65
Alexander Smolin <alexjavacore@mail.ru>
2022-09-13 18:19:03 +0300

    Update PostgreeSQL Select Table.sql

--- PostgreeSQL Select Table.sql
+++ PostgreeSQL Select Table.sql
@@ -2,25 +2,25 @@
 
 SELECT name FROM world
 WHERE population >
-(SELECT population FROM world
-         WHERE name='Russia');
+      (SELECT population FROM world
+       WHERE name='Russia');
 
 -- #2 Show the countries in Europe with a per capita GDP greater than 'United Kingdom'.
 
 SELECT name FROM world
 WHERE continent ='Europe'
 AND gdp/population >
-          (SELECT gdp/population FROM world
-            WHERE name = 'United Kingdom')
+      (SELECT gdp/population FROM world
+       WHERE name = 'United Kingdom')
 
 -- #3 List the name and continent of countries in the continents containing either Argentina or Australia. 
 -- # Order by name of the country.
 
 SELECT name, continent FROM world
 WHERE continent IN 
-           (SELECT continent FROM world
-            WHERE name IN ('Argentina', 'Australia'))
-            ORDER BY name
+      (SELECT continent FROM world
+       WHERE name IN ('Argentina', 'Australia'))
+       ORDER BY name
 
 -- #4. Which country has a population that is more than United Kingom but less than Germany? 
 -- # Show the name and the population.
